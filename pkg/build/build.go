653dafcbddefca1548335bb69eff4f30901ac020
Daniel Hiltgen <hiltgend@vmware.com>
2022-06-30 15:30:42 -0700

    New GRPC init

--- pkg/build/build.go
+++ pkg/build/build.go
@@ -23,6 +23,7 @@ import (
 	"github.com/vmware-tanzu/buildkit-cli-for-kubectl/pkg/imagetools"
 	"github.com/vmware-tanzu/buildkit-cli-for-kubectl/pkg/progress"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
 	"k8s.io/client-go/tools/clientcmd"
 
 	"github.com/docker/distribution/reference"
@@ -1053,7 +1054,7 @@ func newContainerdLoader(ctx context.Context, d driver.Driver, kubeClientConfig 
 	started := make([]chan struct{}, len(nodeNames))
 	for i := range nodeNames {
 		nodeName := nodeNames[i]
-		c, err := containerd.New(nodeName,
+		c, err := containerd.New("/run/containerd/containerd.sock",
 
 			// TODO - plumb the containerd namespace through so this isn't hard-coded
 			containerd.WithDefaultNamespace("k8s.io"),
@@ -1067,11 +1068,11 @@ func newContainerdLoader(ctx context.Context, d driver.Driver, kubeClientConfig 
 						return conn, nil
 					},
 				),
-				grpc.WithInsecure(), // Nested connection on an existing secure transport
+				grpc.WithTransportCredentials(insecure.NewCredentials()), //  Nested connection on an existing secure transport
 			}),
 		)
 		if err != nil {
-			return nil, nil, fmt.Errorf("failed to set up docker client via proxy: %w", err)
+			return nil, nil, fmt.Errorf("failed to set up containerd client via proxy: %w", err)
 		}
 
 		pr, pw := io.Pipe()
